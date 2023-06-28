39590e4cd1704503592a5bcf739b4358b564c37a
Théo <theo.fabianomattei@gmail.com>
2023-06-28 23:14:16 +0200

    ✨ feat: create About component to display information about me

---
+++ src/about.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./css/style.css";
+import "./css/about.css";
+import "./css/properties.css";
+
+function About(): React.ReactElement {
+  return (
+    <section className="section" id="about_section">
+      <div className="content">
+        <span className="section_title">Qui suis-je ?</span>
+        <div className="about_content">
+          <p className="p_1">
+            Je suis un étudiant en informatique passionné par le développement.
+            Actuellement, je suis étudiant à Epitech depuis 2022 où j'ai pu
+            acquérir des compétences solides en programmation en langage C et en
+            gestion de projets.
+            <br />
+            <br />
+            Au cours de mes études, j'ai développé une passion pour la
+            résolution de problèmes complexes. J'aime relever des défis et
+            explorer de nouvelles approches pour créer des solutions innovantes.
+            Je suis également curieux et toujours en quête de nouvelles
+            connaissances pour améliorer mes compétences.
+            <br />
+            <br />
+            J'ai eu l'occasion de réaliser de nombreux projets passionnants au
+            cours de mes études, notamment en intelligence artificielle, dans le
+            développement de jeux vidéo et dans d'autres domaines de
+            l'informatique{" "}
+            <i>
+              (Veuillez consulter ma partie{" "}
+              <a className="link" href="#projects">
+                projet
+              </a>{" "}
+              sur ce site pour plus de détails)
+            </i>
+            . <br />
+            <br />
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
