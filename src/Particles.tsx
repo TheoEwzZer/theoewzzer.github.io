3108ddb9a4ab16e1ce644fafe70e55efbc5ac045
Théo <theo.fabianomattei@gmail.com>
2023-06-29 00:25:39 +0200

    ✨ feat: create Particle component to render particle effects

---
+++ src/Particles.tsx
@@ -0,0 +1,94 @@
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import type { Engine } from "tsparticles-engine";
+
+function Particle(): React.ReactElement {
+  const particlesInit: (engine: Engine) => Promise<void> = useCallback(
+    async (engine: Engine): Promise<void> => {
+      await loadFull(engine);
+    },
+    []
+  );
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        background: {
+          color: {
+            value: "#000000",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "grab",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#197dff",
+            distance: 150,
+            enable: true,
+            opacity: 1,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
+
+export default Particle;
