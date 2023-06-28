01543df42a99a2ee6c4f1b57d68d6971fe89f3d8
Théo <theo.fabianomattei@gmail.com>
2023-06-28 23:42:42 +0200

    ✨ feat: add Contact component to display links to social media

---
+++ src/contact.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./css/style.css";
+import "./css/contact.css";
+import "./css/properties.css";
+
+function Contact(): React.ReactElement {
+  const LinkedIn = {
+    "--size": "150px",
+  } as React.CSSProperties;
+  const github = {
+    "--size": "115px",
+  } as React.CSSProperties;
+  const mail = {
+    "--size": "220px",
+  } as React.CSSProperties;
+
+  return (
+    <section className="section" id="contact_section">
+      <div className="content leaning">
+        <span className="section_title">Comment me contacter ?</span>
+        <div className="contact_content">
+          <div className="social_line">
+            <a
+              className="social_link"
+              href="https://www.linkedin.com/in/theo-fabiano/"
+              target="_blank"
+              style={LinkedIn}
+              rel="noreferrer"
+            >
+              <img
+                src="/shapes/contact/Linkedin.svg"
+                alt="linkedin"
+                width="50px"
+                height="50px"
+              />
+              <div className="social_text">
+                <span>in/Théo-Fabiano</span>
+              </div>
+            </a>
+          </div>
+          <div className="social_line">
+            <a
+              className="social_link"
+              href="https://github.com/TheoEwzZer"
+              target="_blank"
+              style={github}
+              rel="noreferrer"
+            >
+              <img
+                src="/shapes/contact/GitHub.svg"
+                alt="github"
+                width="50px"
+                height="50px"
+              />
+              <div className="social_text">
+                <span>TheoEwzZer</span>
+              </div>
+            </a>
+          </div>
+          <div className="social_line">
+            <div className="social_link" style={mail}>
+              <img
+                src="/shapes/contact/Mail.svg"
+                alt="mail"
+                width="50px"
+                height="50px"
+              />
+              <div className="social_text">
+                <span>
+                  <a href="mailto:theo.fabiano@epitech.eu">
+                    theo.fabiano@epitech.eu
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
