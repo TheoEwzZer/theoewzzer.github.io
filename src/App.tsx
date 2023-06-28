54aa5d1fc142f14bfdb5136609d59e5ee995972c
Théo <theo.fabianomattei@gmail.com>
2023-06-28 23:13:47 +0200

    📝 chore: add About component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./home";
+import About from "./about";
 
 function App(): React.ReactElement {
   return (
     <>
       <Home />
+      <About />
     </>
   );
 }
