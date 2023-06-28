cfa21d92d501b7af765c0d3c12afa92976dfb702
Théo <theo.fabianomattei@gmail.com>
2023-06-28 22:59:45 +0200

    ✨ feat: create Home component

---
+++ src/home.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./css/style.css";
+import "./css/home.css";
+import "./css/properties.css";
+
+function Home(): React.ReactElement {
+  const birthdate = new Date(2004, 11, 15);
+  const age: number = Math.floor(
+    (Date.now() - birthdate.getTime()) / (365.25 * 24 * 60 * 60 * 1000)
+  );
+
+  return (
+    <section className="section" id="home_section">
+      <div id="particles"></div>
+      <div className="content">
+        <span className="hi">Je m'appelle</span>
+        <span className="name">Théo Fabiano.</span>
+        <p className="bio">
+          Je suis un étudiant français de {age} ans à l'école d'informatique
+          <a
+            className="link"
+            href="https://www.epitech.eu/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Epitech Technology
+          </a>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
