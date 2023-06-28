33e927b071c46c14a78d050ae1fb0d11ab5ab8f5
Théo <theo.fabianomattei@gmail.com>
2023-06-29 01:25:22 +0200

    🎉 feat: add Skills component to App to display skills section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import About from "./about";
 import Contact from "./contact";
 import Home from "./home";
+import Skills from "./skills";
 
 function App(): React.ReactElement {
   return (
     <>
       <Home />
       <About />
+      <Skills />
       <Contact />
     </>
   );
