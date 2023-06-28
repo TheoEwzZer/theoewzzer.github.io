aa5f0944bfb9cf2053319b1fecaec694dbaa8c19
Théo <theo.fabianomattei@gmail.com>
2023-06-28 23:43:52 +0200

    ✨ feat(App.tsx): add Contact component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./home";
 import About from "./about";
+import Contact from "./contact";
 
 function App(): React.ReactElement {
   return (
     <>
       <Home />
       <About />
+      <Contact />
     </>
   );
 }
