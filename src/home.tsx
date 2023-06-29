3075174e95facdd462005dad9adb4c204053e271
Théo <theo.fabianomattei@gmail.com>
2023-06-29 22:15:18 +0200

    ♻️ refactor: refactor code to improve readability and maintainability 💫 feat: add animations to home section elements for a better user experience 🔧 fix: adjust home section height based on content to prevent overflow

--- src/home.tsx
+++ src/home.tsx
@@ -4,12 +4,62 @@ import "./css/properties.css";
 import "./css/style.css";
 
 import Particles from "./Particles";
+import { is_in_viewport, sleep } from "./utils";
 
 function Home(): React.ReactElement {
   const birthdate = new Date(2004, 11, 15);
   const age: number = Math.floor(
     (Date.now() - birthdate.getTime()) / (365.25 * 24 * 60 * 60 * 1000)
   );
+
+  const home_in_animations: () => void = (): void => {
+    let done: boolean = false;
+    let elements: NodeListOf<HTMLElement> = document.querySelectorAll(
+      "#home_section .in_animation"
+    );
+    let section: HTMLElement | null = document.querySelector("#home_section");
+
+    const in_animation_check: () => Promise<void> = async (): Promise<void> => {
+      if (!done && is_in_viewport(section)) {
+        for (let i of elements) {
+          i.style.opacity = "1";
+          i.style.transform = "translateY(0)";
+          await sleep(300);
+        }
+
+        done = true;
+      }
+    };
+
+    window.addEventListener("scroll", (): void => {
+      in_animation_check();
+    });
+
+    window.addEventListener("resize", (): void => {
+      in_animation_check();
+    });
+
+    in_animation_check();
+  };
+
+  const home_events: () => void = (): void => {
+    const content: HTMLElement | null = document.querySelector(
+      "#home_section .content"
+    );
+    if (content) {
+      const rect: DOMRect = content.getBoundingClientRect();
+      const section: HTMLElement | null =
+        document.querySelector("#home_section");
+      if (section) {
+        section.style.minHeight = rect.height + 90 + "px";
+      }
+    }
+  };
+
+  React.useEffect((): void => {
+    home_in_animations();
+    home_events();
+  }, []);
 
   return (
     <>
@@ -21,9 +71,9 @@ function Home(): React.ReactElement {
           <Particles />
         </div>
         <div className="content">
-          <span className="hi">Je m'appelle</span>
-          <span className="name">Théo Fabiano.</span>
-          <p className="bio">
+          <span className="hi in_animation">Je m'appelle</span>
+          <span className="name in_animation">Théo Fabiano.</span>
+          <p className="bio in_animation">
             Je suis un étudiant français de {age} ans à l'école d'informatique
             <a
               className="link"
@@ -34,6 +84,21 @@ function Home(): React.ReactElement {
               Epitech Technology
             </a>
           </p>
+          <div className="home_button in_animation">
+            <a className="button" href="#about">
+              <p>Commencer</p>
+              <svg
+                className="button_arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 17.69 17.39"
+              >
+                <g>
+                  <path className="path_1" d="M8.9 12.4 L8.9 12.4" />
+                  <path className="path_2" d="M16.2 5 8.9 12.4 1.5 5" />
+                </g>
+              </svg>
+            </a>
+          </div>
         </div>
       </section>
     </>
