46adf756d738f1fcc60feaaedbdbaa9c38a8ac85
Théo <theo.fabianomattei@gmail.com>
2023-06-29 03:55:11 +0200

    🔧 fix: add Projects component to render in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Contact from "./contact";
 import Home from "./home";
 import Skills from "./skills";
 import Footer from "./footer";
+import Projects from "./projects";
 
 function App(): React.ReactElement {
   return (
     <>
       <Home />
       <About />
+      <Projects />
       <Skills />
       <Contact />
       <Footer />
