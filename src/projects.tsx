33d70d5d728049f8b8440fa0740189d8c956da28
Théo <theo.fabianomattei@gmail.com>
2023-07-01 00:06:21 +0200

    📱 fix: fix projets sort not working on mobile

--- src/projects.tsx
+++ src/projects.tsx
@@ -84,165 +84,152 @@ function Projects(): React.ReactElement {
         project_date =
           project_date.charAt(0).toUpperCase() + project_date.slice(1);
       }
-      if (window.innerWidth < 780) {
-        const projectsContent: HTMLElement | null = document.querySelector(
-          "#projects_section .projects_content"
-        );
-        if (projectsContent != null) {
-          projectsContent.innerHTML +=
-            `
-        <div class="project ${
-          is_safari() ? "safari_fix" : ""
-        }" style="background-image: url(${project.image});">
-          <div class="project_text"">
-            <div class="type">
-              <span>${project.type}</span>
-              <span>•</span>
-              <span>${project_date}</span>
-            </div>
-            <a class="project_title" href="${
-              project.links[0]
-            }" target="_blank">${project.title}</a>
-            <div class="text"><p>${project.description}</p></div>
-            <div class="tags">` +
-            tags +
-            `</div>
-            <div class="links">` +
-            (project.links[1] === "none"
-              ? ""
-              : `
-              <a class="github" href="${project.links[1]}" target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.05 20.31"><g><path d="M7.26 16.34c-4.11 1.23-4.11-2.06-5.76-2.47M13 18.81V15.62a2.78 2.78 0 0 0-.77-2.15c2.59-.28 5.3-1.26 5.3-5.76a4.46 4.46 0 0 0-1.23-3.08 4.18 4.18 0 0 0-.08-3.11s-1-.29-3.22 1.22a11 11 0 0 0-5.76 0C5 1.23 4 1.52 4 1.52A4.18 4.18 0 0 0 4 4.63 4.48 4.48 0 0 0 2.73 7.74c0 4.46 2.72 5.44 5.31 5.76a2.8 2.8 0 0 0-.78 2.12v3.19"/></g></svg>
-                <span class="bubble">Voir le code</span>
-              </a>
-              `) +
-            (project.links[2] === "none"
-              ? ""
-              : `
-              <a class="test" href="${project.links[2]}" target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.09 18.64"><g><path d="M14.55 7.52 4.62 1.78A2.08 2.08 0 0 0 1.5 3.58V15.05a2.08 2.08 0 0 0 3.12 1.8l9.93-5.73A2.08 2.08 0 0 0 14.55 7.52Z"/></g></svg>
-                <span class="bubble">Tester le programme</span>
-              </a>
-              `) +
-            `
-            </div>
-          </div>
-        </div>`;
-        }
-      } else if (featured) {
+
+      const projectsContent: HTMLElement | null = document.querySelector(
+        "#projects_section .projects_content"
+      );
+
+      const otherProjectsContent: HTMLElement | null = document.querySelector(
+        "#projects_section .other_projects_content"
+      );
+
+      if (featured) {
         for (let tag of project.tags)
           tags += `<a href="${tag.url}" target="_blank">${tag.name}</a>`;
 
-        const projectsContent: HTMLElement | null = document.querySelector(
-          "#projects_section .projects_content"
-        );
-
-        if (projectsContent != null) {
+        if (window.innerWidth > 780 && projectsContent) {
           projectsContent.innerHTML +=
             `
-                    <div class="in_animation project ${
-                      inverted ? "inverted" : ""
-                    }">
-                        <div class="project_text">
-                            <div class="type">
-                                <span>${project_date}</span>
-                                <span>•</span>
-                                <span>${project.type}</span>
-                            </div>
-                            <a class="project_title" href="${
-                              project.links[0]
-                            }" target="_blank">${project.title}</a>
-                            <div class="text"><p>${
-                              project.description
-                            }</p></div>
-                            <div class="tags">` +
+					<div class="in_animation project ${inverted ? "inverted" : ""}">
+						<div class="project_text">
+							<div class="type">
+								<span>${project_date}</span>
+								<span>•</span>
+								<span>${project.type}</span>
+							</div>
+							<a class="project_title" href="${project.links[0]}" target="_blank">${
+              project.title
+            }</a>
+							<div class="text"><p>${project.description}</p></div>
+							<div class="tags">` +
             tags +
             `</div>
-                            <div class="links">` +
+							<div class="links">` +
             (project.links[1] === "none"
               ? ""
               : `
-                                <a class="github" href="${project.links[1]}" target="_blank">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.05 20.31"><g><path d="M7.26 16.34c-4.11 1.23-4.11-2.06-5.76-2.47M13 18.81V15.62a2.78 2.78 0 0 0-.77-2.15c2.59-.28 5.3-1.26 5.3-5.76a4.46 4.46 0 0 0-1.23-3.08 4.18 4.18 0 0 0-.08-3.11s-1-.29-3.22 1.22a11 11 0 0 0-5.76 0C5 1.23 4 1.52 4 1.52A4.18 4.18 0 0 0 4 4.63 4.48 4.48 0 0 0 2.73 7.74c0 4.46 2.72 5.44 5.31 5.76a2.8 2.8 0 0 0-.78 2.12v3.19"/></g></svg>
-                                    <span class="bubble">Voir le code</span>
-                                </a>
-                                `) +
+								<a class="github" href="${project.links[1]}" target="_blank">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.05 20.31"><g><path d="M7.26 16.34c-4.11 1.23-4.11-2.06-5.76-2.47M13 18.81V15.62a2.78 2.78 0 0 0-.77-2.15c2.59-.28 5.3-1.26 5.3-5.76a4.46 4.46 0 0 0-1.23-3.08 4.18 4.18 0 0 0-.08-3.11s-1-.29-3.22 1.22a11 11 0 0 0-5.76 0C5 1.23 4 1.52 4 1.52A4.18 4.18 0 0 0 4 4.63 4.48 4.48 0 0 0 2.73 7.74c0 4.46 2.72 5.44 5.31 5.76a2.8 2.8 0 0 0-.78 2.12v3.19"/></g></svg>
+									<span class="bubble">Voir le code</span>
+								</a>
+								`) +
             (project.links[2] === "none"
               ? ""
               : `
-                                <a class="test" href="${project.links[2]}" target="_blank">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.09 18.64"><g><path d="M14.55 7.52 4.62 1.78A2.08 2.08 0 0 0 1.5 3.58V15.05a2.08 2.08 0 0 0 3.12 1.8l9.93-5.73A2.08 2.08 0 0 0 14.55 7.52Z"/></g></svg>
-                                    <span class="bubble">Tester le programme</span>
-                                </a>
-                                `) +
+								<a class="test" href="${project.links[2]}" target="_blank">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.09 18.64"><g><path d="M14.55 7.52 4.62 1.78A2.08 2.08 0 0 0 1.5 3.58V15.05a2.08 2.08 0 0 0 3.12 1.8l9.93-5.73A2.08 2.08 0 0 0 14.55 7.52Z"/></g></svg>
+									<span class="bubble">Tester le programme</span>
+								</a>
+								`) +
             `
-                            </div>
-                        </div>
-                        <div class="project_view">
-            <a ${is_safari() ? 'class="safari_fix"' : ""} href="${
+							</div>
+						</div>
+						<div class="project_view">
+							<a ${is_safari() ? 'class="safari_fix"' : ""} href="${
               project.links[0]
             }" target="_blank">
-              ${
-                project.image !== "none"
-                  ? `
-                                <img src="${
-                                  project.image
-                                }" alt="${project.title.toLowerCase()} image" width="1440px" height="810px"/>`
-                  : ""
-              }
-                            </a>
-                        </div>
-                    </div>`;
-        }
-      } else {
-        for (let tag of project.tags) tags += `<span>${tag.name}</span>`;
-
-        const otherProjectsContent: HTMLElement | null = document.querySelector(
-          "#projects_section .other_projects_content"
-        );
-
-        if (otherProjectsContent != null) {
-          otherProjectsContent.innerHTML +=
+								<img src="${
+                  project.image
+                }" alt="${project.title.toLowerCase()} image" width="1440px" height="810px"/>
+								` +
             `
-                <div class="in_animation other_project">
-                    <div class="other_project_content">
-                        <div class="header">
-                            <div class="logos">
-              <div class="other_project_text">
-              <div class="other_type">
-                <span>${project_date}</span>
-                <span>•</span>
-                <span>${project.type}</span>
-              </div>
-            </div>
-                                <div class="links">
-                                    ` +
+							</a>
+						</div>
+					</div>`;
+        } else if (projectsContent) {
+          projectsContent.innerHTML +=
+            `
+					<div class="in_animation project ${
+            is_safari() ? "safari_fix" : ""
+          }" style="background-image: url(${project.image});">
+						<div class="project_text"">
+							<div class="type">
+								<span>${project.type}</span>
+								<span>•</span>
+								<span>${project_date}</span>
+							</div>
+							<a class="project_title" href="${project.links[0]}" target="_blank">${
+              project.title
+            }</a>
+							<div class="text"><p>${project.description}</p></div>
+							<div class="tags">` +
+            tags +
+            `</div>
+							<div class="links">` +
             (project.links[1] === "none"
               ? ""
               : `
-                                    <a class="github" href="${project.links[1]}" aria-label="github" target="_blank">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.05 20.31"><g><path d="M7.26 16.34c-4.11 1.23-4.11-2.06-5.76-2.47M13 18.81V15.62a2.78 2.78 0 0 0-.77-2.15c2.59-.28 5.3-1.26 5.3-5.76a4.46 4.46 0 0 0-1.23-3.08 4.18 4.18 0 0 0-.08-3.11s-1-.29-3.22 1.22a11 11 0 0 0-5.76 0C5 1.23 4 1.52 4 1.52A4.18 4.18 0 0 0 4 4.63 4.48 4.48 0 0 0 2.73 7.74c0 4.46 2.72 5.44 5.31 5.76a2.8 2.8 0 0 0-.78 2.12v3.19"/></g></svg>
-                                    </a>
-                                    `) +
+								<a class="github" href="${project.links[1]}" target="_blank">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.05 20.31"><g><path d="M7.26 16.34c-4.11 1.23-4.11-2.06-5.76-2.47M13 18.81V15.62a2.78 2.78 0 0 0-.77-2.15c2.59-.28 5.3-1.26 5.3-5.76a4.46 4.46 0 0 0-1.23-3.08 4.18 4.18 0 0 0-.08-3.11s-1-.29-3.22 1.22a11 11 0 0 0-5.76 0C5 1.23 4 1.52 4 1.52A4.18 4.18 0 0 0 4 4.63 4.48 4.48 0 0 0 2.73 7.74c0 4.46 2.72 5.44 5.31 5.76a2.8 2.8 0 0 0-.78 2.12v3.19"/></g></svg>
+									<span class="bubble">Voir le code</span>
+								</a>
+								`) +
             (project.links[2] === "none"
               ? ""
               : `
-                                    <a class="test" href="${project.links[2]}" aria-label="test" target="_blank">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.09 18.64"><g><path d="M14.55 7.52 4.62 1.78A2.08 2.08 0 0 0 1.5 3.58V15.05a2.08 2.08 0 0 0 3.12 1.8l9.93-5.73A2.08 2.08 0 0 0 14.55 7.52Z"/></g></svg>
-                                    </a>
-                                    `) +
+								<a class="test" href="${project.links[2]}" target="_blank">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.09 18.64"><g><path d="M14.55 7.52 4.62 1.78A2.08 2.08 0 0 0 1.5 3.58V15.05a2.08 2.08 0 0 0 3.12 1.8l9.93-5.73A2.08 2.08 0 0 0 14.55 7.52Z"/></g></svg>
+									<span class="bubble">Tester le programme</span>
+								</a>
+								`) +
             `
-                                </div>
-                            </div>
-                            <a href="${project.links[0]}" target="_blank" class="project_title">${project.title}</a>
-                            <p class="text">${project.description}</p>
-                        </div>
-                        <div class="tags">` +
-            tags +
-            `</div>
-                    </div>
-                </div>`;
+							</div>
+						</div>
+					</div>`;
         }
+      } else if (otherProjectsContent) {
+        for (let tag of project.tags) tags += `<span>${tag.name}</span>`;
+
+        otherProjectsContent.innerHTML +=
+          `
+				<div class="in_animation other_project">
+					<div class="other_project_content">
+						<div class="header">
+							<div class="logos">
+              <div class="other_project_text">
+                <div class="other_type">
+                  <span>${project_date}</span>
+                  <span>•</span>
+                  <span>${project.type}</span>
+                </div>
+              </div>
+								<div class="links">
+									` +
+          (project.links[1] === "none"
+            ? ""
+            : `
+									<a class="github" href="${project.links[1]}" aria-label="github" target="_blank">
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.05 20.31"><g><path d="M7.26 16.34c-4.11 1.23-4.11-2.06-5.76-2.47M13 18.81V15.62a2.78 2.78 0 0 0-.77-2.15c2.59-.28 5.3-1.26 5.3-5.76a4.46 4.46 0 0 0-1.23-3.08 4.18 4.18 0 0 0-.08-3.11s-1-.29-3.22 1.22a11 11 0 0 0-5.76 0C5 1.23 4 1.52 4 1.52A4.18 4.18 0 0 0 4 4.63 4.48 4.48 0 0 0 2.73 7.74c0 4.46 2.72 5.44 5.31 5.76a2.8 2.8 0 0 0-.78 2.12v3.19"/></g></svg>
+									</a>
+									`) +
+          (project.links[2] === "none"
+            ? ""
+            : `
+									<a class="test" href="${project.links[2]}" aria-label="test" target="_blank">
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.09 18.64"><g><path d="M14.55 7.52 4.62 1.78A2.08 2.08 0 0 0 1.5 3.58V15.05a2.08 2.08 0 0 0 3.12 1.8l9.93-5.73A2.08 2.08 0 0 0 14.55 7.52Z"/></g></svg>
+									</a>
+									`) +
+          `
+								</div>
+							</div>
+							<a href="${project.links[0]}" target="_blank" class="project_title">${project.title}</a>
+							<p class="text">${project.description}</p>
+						</div>
+						<div class="tags">` +
+          tags +
+          `</div>
+					</div>
+				</div>`;
       }
     }
   };
